0f2003e29bb18284c48be446c8268eced37a6690
FaelTwk <138951101+FaelTwk@users.noreply.github.com>
2025-11-05 13:21:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,10 @@ let desligada = true;
 lamp.addEventListener('click', () => {
     if(desligada){
         lamp.src = "assets/lamp_on.png";
-        // CORREÇÃO: Removido "background: " da string
         document.body.style.background = "radial-gradient(circle, white 8%, yellow 100%)";
         desligada = false;
     }else{
         lamp.src = "assets/lamp_off.png";
-        // CORREÇÃO: Removido "background: " da string
         document.body.style.background = "radial-gradient(circle, white 8%, black 100%)";
         desligada = true;
     }
