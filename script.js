7de20871111fe684cd44f8328d189cf6a2019970
FaelTwk <138951101+FaelTwk@users.noreply.github.com>
2025-11-05 13:19:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-var lamp = document.getElementById("lamp");
-let desligada = true
+const lamp = document.getElementById("lamp");
+let desligada = true;
 
 lamp.addEventListener('click', () => {
     if(desligada){
-        lamp.src = "assets/lamp_on.png"
-        document.body.style.background = "background: radial-gradient(circle, white 8%, yellow 100%);"
+        lamp.src = "assets/lamp_on.png";
+        // CORREÇÃO: Removido "background: " da string
+        document.body.style.background = "radial-gradient(circle, white 8%, yellow 100%)";
         desligada = false;
     }else{
-        lamp.src = "assets/lamp_off.png"
-        document.body.style.background = "background: radial-gradient(circle, white 8%, black 100%);"
+        lamp.src = "assets/lamp_off.png";
+        // CORREÇÃO: Removido "background: " da string
+        document.body.style.background = "radial-gradient(circle, white 8%, black 100%)";
         desligada = true;
     }
-})
-
-
+});
